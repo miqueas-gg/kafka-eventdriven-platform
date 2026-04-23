fbb53a2be4e2b29cf5981d4883c900f48f6836f1
SaulHdez <ivhsaulhdez@gmail.com>
2026-04-23 13:50:40 +0100

    feat:"Issue12"

--- domain-service/src/main/java/com/kafkaeventdriven/domain/entities/Product.java
+++ domain-service/src/main/java/com/kafkaeventdriven/domain/entities/Product.java
@@ -15,7 +15,7 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
-    private int stock;
+    private Integer stock;
     private String category;
 
     @Enumerated(EnumType.STRING)

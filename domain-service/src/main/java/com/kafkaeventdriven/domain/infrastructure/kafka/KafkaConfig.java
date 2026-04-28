50f84814c0a3d00195c691dda7ed370867a931c3
SaulHdez <ivhsaulhdez@gmail.com>
2026-04-28 12:49:02 +0100

    fix:"Arreglamso algunos detalles"

--- domain-service/src/main/java/com/kafkaeventdriven/domain/infrastructure/kafka/KafkaConfig.java
+++ domain-service/src/main/java/com/kafkaeventdriven/domain/infrastructure/kafka/KafkaConfig.java
@@ -12,13 +12,10 @@ public class KafkaConfig {
 
     @Bean
     public DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> template) {
-        // 1. El "Recuperador": Si el mensaje falla, lo envía al topic original + ".dlt"
-        // En tu caso, de "domain.events" lo mandará a "domain.events.dlt"
+     
         DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template);
 
-        // 2. Política de reintentos: 
-        // 1000L = espera 1 segundo entre intentos.
-        // 2L = haz 2 reintentos (total 3 intentos: el original + 2 extras).
+        
         return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 2L));
     }
 }

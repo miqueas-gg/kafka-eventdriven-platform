32c05eff9fbaa11932cb87ffa7f21ac8c80228b0
SaulHdez <ivhsaulhdez@gmail.com>
2026-04-29 12:47:16 +0100

    feat:"Creamos la Issue 18"

---
+++ event-ingest-service/src/main/java/com/kafkaeventdriven/ingest/application/dtos/EventResponseDTO.java
@@ -0,0 +1,14 @@
+package com.kafkaeventdriven.ingest.application.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record EventResponseDTO(
+    UUID id,                // ID interno (Primary Key)
+    UUID eventId,           // ID de negocio (el de Kafka)
+    String eventType,
+    String source,
+    Object payload,         // El JSONB completo
+    LocalDateTime occurredAt,
+    LocalDateTime ingestedAt
+) {}
--- event-ingest-service/src/main/java/com/kafkaeventdriven/ingest/domain/repositories/EventRepository.java
+++ event-ingest-service/src/main/java/com/kafkaeventdriven/ingest/domain/repositories/EventRepository.java
@@ -1,13 +1,27 @@
 package com.kafkaeventdriven.ingest.domain.repositories;
 
+import com.kafkaeventdriven.ingest.application.dtos.EventStatsDTO;
 import com.kafkaeventdriven.ingest.domain.entities.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface EventRepository extends JpaRepository<EventEntity, UUID> {
-    
+public interface EventRepository extends JpaRepository<EventEntity, UUID>, JpaSpecificationExecutor<EventEntity> {
+// Mantener para Idempotencia (Issue anterior)
     boolean existsByEventId(UUID eventId);
+
+    // Nuevo: Buscar por el UUID de negocio
+    Optional<EventEntity> findByEventId(UUID eventId);
+
+    // Nuevo: Estadísticas agregadas
+    @Query("SELECT new com.kafkaeventdriven.ingest.application.dtos.EventStatsDTO(e.eventType, COUNT(e)) " +
+           "FROM EventEntity e " +
+           "GROUP BY e.eventType")
+    List<EventStatsDTO> getStatsByType();
 }
